d4e97d7ad5a1ac87aa4822b098c45af88a2a439a
Zeonsi <tomasgarrido0212@gmial.com>
2023-10-16 09:33:40 -0300

    first commit

---
+++ core/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model):
+    id_usuario = models.IntegerField(primary_key=True, verbose_name='Id del Usuario')
+    nombre_usuario = models.CharField(max_length=50, verbose_name='Nombre del Usuario')
+    direccion_usuario = models.CharField(max_length=50, verbose_name='Direccion del Usuario')
+    def __str__(self):
+        return self.nombre_usuario
