5d443e019093afde65c1b0909acf4f92c4295a42
Zeonsi <tomasgarrido0212@gmial.com>
2023-10-16 18:48:39 -0300

    prueba

--- core/models.py
+++ core/models.py
@@ -5,5 +5,6 @@ class Usuario(models.Model):
     id_usuario = models.IntegerField(primary_key=True, verbose_name='Id del Usuario')
     nombre_usuario = models.CharField(max_length=50, verbose_name='Nombre del Usuario')
     direccion_usuario = models.CharField(max_length=50, verbose_name='Direccion del Usuario')
+    password_usuario = models.CharField(max_length=50, verbose_name='Contrasenna del Usuario')
     def __str__(self):
         return self.nombre_usuario
