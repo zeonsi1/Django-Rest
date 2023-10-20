3c64be737999ebe5ff79d4c2d240e4064d8d2053
TiBu132 <jfbadilla.mo@hotmail.com>
2023-10-19 23:44:01 -0300

    Commit

--- rest_asistencia/views.py
+++ rest_asistencia/views.py
@@ -6,6 +6,7 @@ from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 from core.models import Usuario
 from .serializers import UsuarioSerializer
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @csrf_exempt
@@ -16,3 +17,20 @@ def lista_usuarios(request):
     serializer = UsuarioSerializer(usuario, many=True)
     return Response(serializer.data)    
 
+@api_view(['GET','PUT'])
+def update_usuarios(request, nombre_usuario):
+    try:
+        usuario = Usuario.objects.get(nombre_usuario=nombre_usuario)
+    except Usuario.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = UsuarioSerializer(usuario, many=True)
+        return Response(serializer.data) 
+    if request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = UsuarioSerializer(usuario, data=data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data) 
+        else:
+            return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
